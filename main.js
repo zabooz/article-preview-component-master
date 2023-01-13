5bdbcc2cf824d78cd58ccc68fa97638bbd23fc12
zabooz <zabooz1988@gmail.com>
2023-01-13 18:43:49 +0100

    lsdkagj

---
+++ main.js
@@ -0,0 +1,28 @@
+const  shareBtn = document.querySelector('.share-btn');
+const michelle    = document.querySelector('.avatar-michelle');
+const shareCon  = document.querySelector('.share-container');
+const footer = document.querySelector('footer');
+const icon = document.querySelector('.share-icon')
+let width = window.innerWidth;
+
+
+
+
+
+
+shareBtn.addEventListener('click',e => {
+    if(width <376){
+        shareBtn.classList.toggle('btn-clicked');
+        shareCon.classList.toggle('share-container-show');
+        michelle.classList.toggle('michelle-hide');
+        footer.classList.toggle('footer-change');
+        icon.classList.toggle('filter')
+    }else{
+        shareBtn.classList.toggle('btn-clicked');
+        icon.classList.toggle('filter');
+        shareCon.classList.toggle('share-container-show-big')
+
+
+    }
+
+})
